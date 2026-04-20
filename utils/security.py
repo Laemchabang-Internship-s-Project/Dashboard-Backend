6c6ac22d8f46c575439736b922dbb0a856ea87b3
Pongpipat <pongpipatongart.m1@gmail.com>
2026-04-20 12:18:29 +0700

    fix docker compose

--- utils/security.py
+++ utils/security.py
@@ -2,7 +2,7 @@ import os
 from fastapi import HTTPException, Security, status
 from fastapi.security.api_key import APIKeyHeader
 
-API_KEY_NAME = "access_token"
+API_KEY_NAME = "x-api-key"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
 
 async def get_api_key(api_key_header: str = Security(api_key_header)):
